8c06c0aa557a4a571dbec11bef4aaf5b7ab71bdc
houke <13581836891@163.com>
2018-09-06 18:43:05 +0800

    微博cell topview  布局完成

---
+++ Swift_Weibo/Swift_Weibo/Classes/View/Home/StatusCell/StatusCellBottomView.swift
@@ -0,0 +1,21 @@
+//
+//  StatusCellBottomView.swift
+//  Swift_Weibo
+//
+//  Created by houke on 2018/9/6.
+//  Copyright © 2018年 houke. All rights reserved.
+//
+
+import UIKit
+
+class StatusCellBottomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
